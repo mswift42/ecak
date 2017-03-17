b1748f9feddff44d6faec635c9fb52a93c915294
mswift42 <martin.haesler@gmail.com>
2017-03-17 14:47:02 +0000

    Change type of image to int.

--- app/src/main/java/mswift42/com/github/eggcrackin/Recipe.kt
+++ app/src/main/java/mswift42/com/github/eggcrackin/Recipe.kt
@@ -2,6 +2,6 @@ package mswift42.com.github.eggcrackin
 
 data class Recipe(val publisher: String, val title: String,
                   val source_url: String,
-                  val image_url: String, val publisher_url: String,
+                  val image_url: Int, val publisher_url: String,
                   val recipe_id: String,
                   val ingredients: List<String>)
