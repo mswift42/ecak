395ccafcf686e3f8fd4811303266435c6885d5d1
mswift42 <martin.haesler@gmail.com>
2017-03-19 10:57:45 +0000

    Change MockRecipeData to companion object.

--- app/src/main/java/mswift42/com/github/eggcrackin/MockRecipeData.kt
+++ app/src/main/java/mswift42/com/github/eggcrackin/MockRecipeData.kt
@@ -1,87 +1,90 @@
 package mswift42.com.github.eggcrackin
 
-val MockRecipeData: List<Recipe> = listOf(
-        Recipe(
-                "The Pioneer Woman",
-                "French Toast",
-                "http://www.simplyrecipes.com/recipes/french_toast/",
-                R.drawable.frenchtoast300200,
-                "http://www.simplyrecipes.com",
-                "36355",
-                listOf(
-                        "4 eggs",
-                        "2/3 cup milk",
-                        "2 teaspoons of cinnamon",
-                        "8 thick slices of 2-day-old bread",
-                        "Butter",
-                        "Maple syrup",
-                        "2 teaspoons freshly grated orange zest"
-                )
-        ),
-        Recipe(
-                "The Pioneer Woman",
-                "French Onion Soup Stuffed Mushrooms",
-                "http://thepioneerwoman.com/cooking/2009/11/french-onion-soup-stuffed-mushrooms/",
-                R.drawable.frenchonionstuffedmushrooms,
-                "http://thepioneerwoman.com",
-                "47078",
-                listOf(
-                        "2 Tablespoons Butter",
-                        "2 whole Large Onions, Halved And Sliced Thin",
-                        "1/4 cub Beef Broth",
-                        "7 dashes Worcestershire Sauce",
-                        "Splash Of Red Or White Wine"
-                )
+object MockRecipeData {
+        var mockRecipeData: MutableList<Recipe> = mutableListOf(
+                Recipe(
+                        "The Pioneer Woman",
+                        "French Toast",
+                        "http://www.simplyrecipes.com/recipes/french_toast/",
+                        R.drawable.frenchtoast300200,
+                        "http://www.simplyrecipes.com",
+                        "36355",
+                        listOf(
+                                "4 eggs",
+                                "2/3 cup milk",
+                                "2 teaspoons of cinnamon",
+                                "8 thick slices of 2-day-old bread",
+                                "Butter",
+                                "Maple syrup",
+                                "2 teaspoons freshly grated orange zest"
+                        )
+                ),
+                Recipe(
+                        "The Pioneer Woman",
+                        "French Onion Soup Stuffed Mushrooms",
+                        "http://thepioneerwoman.com/cooking/2009/11/french-onion-soup-stuffed-mushrooms/",
+                        R.drawable.frenchonionstuffedmushrooms,
+                        "http://thepioneerwoman.com",
+                        "47078",
+                        listOf(
+                                "2 Tablespoons Butter",
+                                "2 whole Large Onions, Halved And Sliced Thin",
+                                "1/4 cub Beef Broth",
+                                "7 dashes Worcestershire Sauce",
+                                "Splash Of Red Or White Wine"
+                        )
 
-        ),
-        Recipe(
-                "All Recipes",
-                "Easy Slow Cooker French Dip",
-                "http://allrecipes.com/Recipe/Easy-Slow-Cooker-French-Dip/Detail.aspx",
-                R.drawable.easyslowcookerfrenchdip,
-                "http://allrecipes.com",
-                "12360",
-                listOf(
-                        "4 pounds rump roast",
-                        "1 can beef broth",
-                        "1 can condensed French onion soup",
-                        "1 bottle beer",
-                        "6 french rolls",
-                        "2 tablespoons butter"
-                )
+                ),
+                Recipe(
+                        "All Recipes",
+                        "Easy Slow Cooker French Dip",
+                        "http://allrecipes.com/Recipe/Easy-Slow-Cooker-French-Dip/Detail.aspx",
+                        R.drawable.easyslowcookerfrenchdip,
+                        "http://allrecipes.com",
+                        "12360",
+                        listOf(
+                                "4 pounds rump roast",
+                                "1 can beef broth",
+                                "1 can condensed French onion soup",
+                                "1 bottle beer",
+                                "6 french rolls",
+                                "2 tablespoons butter"
+                        )
 
-        ),
-        Recipe(
-                "Closet Cooking",
-                "Pumpkin Pie French Toast",
-                "http://www.closetcooking.com/2009/11/pumpkin-pie-french-toast.html",
-                R.drawable.pumpkinpiefrenchtoast,
-                "http://closetcooking.com",
-                "35499",
-                listOf(
-                        "2 eggs",
-                        "1/4 cup milk",
-                        "1/4 cup pumpkin puree",
-                        "1/4 teaspoon vanilla",
-                        "1/2 teaspoon cinnamon",
-                        "1/4 teaspoon ginger",
-                        "1/8 teaspoon cloves",
-                        "1/8 teaspoon nutmeg",
-                        "2 tablespoons brown sugar",
-                        "8 slices of bread"
-                )
-        ),
-        Recipe(
-                "The Pioneer Woman",
-                "Perfect French Fries",
-                "http://thepioneerwoman.com/cooking/2012/01/perfect-french-fries/",
-                R.drawable.frenchfries,
-                "http://thepioneerwoman.com",
-                "46959",
-                listOf(
-                        "5 pounds Russet Potatoes",
-                        "Vegetable Or Peanut Oil For Frying",
-                        "Sea Salt"
+                ),
+                Recipe(
+                        "Closet Cooking",
+                        "Pumpkin Pie French Toast",
+                        "http://www.closetcooking.com/2009/11/pumpkin-pie-french-toast.html",
+                        R.drawable.pumpkinpiefrenchtoast,
+                        "http://closetcooking.com",
+                        "35499",
+                        listOf(
+                                "2 eggs",
+                                "1/4 cup milk",
+                                "1/4 cup pumpkin puree",
+                                "1/4 teaspoon vanilla",
+                                "1/2 teaspoon cinnamon",
+                                "1/4 teaspoon ginger",
+                                "1/8 teaspoon cloves",
+                                "1/8 teaspoon nutmeg",
+                                "2 tablespoons brown sugar",
+                                "8 slices of bread"
+                        )
+                ),
+                Recipe(
+                        "The Pioneer Woman",
+                        "Perfect French Fries",
+                        "http://thepioneerwoman.com/cooking/2012/01/perfect-french-fries/",
+                        R.drawable.frenchfries,
+                        "http://thepioneerwoman.com",
+                        "46959",
+                        listOf(
+                                "5 pounds Russet Potatoes",
+                                "Vegetable Or Peanut Oil For Frying",
+                                "Sea Salt"
+                        )
                 )
         )
-)
+
+}
