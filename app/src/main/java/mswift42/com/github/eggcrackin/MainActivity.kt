5ea101167c63b8bf74c1ac437c7e8f804b352428
mswift42 <martin.haesler@gmail.com>
2017-03-19 16:01:58 +0000

    Set layoutManager for recyclerview.

--- app/src/main/java/mswift42/com/github/eggcrackin/MainActivity.kt
+++ app/src/main/java/mswift42/com/github/eggcrackin/MainActivity.kt
@@ -2,11 +2,16 @@ package mswift42.com.github.eggcrackin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val layoutManager = LinearLayoutManager(this)
+        ec_recyclerview.layoutManager = layoutManager
     }
 }
