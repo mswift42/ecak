3b8d2b96ea2ad96c0f7973464e4aee0bb8e921a8
mswift42 <martin.haesler@gmail.com>
2017-03-21 11:29:30 +0000

    Use Gridlayoutmanager for recyclerview.

--- app/src/main/java/mswift42/com/github/eggcrackin/MainActivity.kt
+++ app/src/main/java/mswift42/com/github/eggcrackin/MainActivity.kt
@@ -2,6 +2,7 @@ package mswift42.com.github.eggcrackin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,7 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val layoutManager = LinearLayoutManager(this)
+        val layoutManager = GridLayoutManager(this, SpanUtility.calculateNoOfColumns(this))
         ec_recyclerview.layoutManager = layoutManager
+        val adapter = RecipesAdapter(MockRecipeData.mockRecipeData)
+        ec_recyclerview.adapter = adapter
     }
 }
