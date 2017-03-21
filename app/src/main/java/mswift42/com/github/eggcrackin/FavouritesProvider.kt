698328b6df8d1ce2381f7df8ed1833dbeb57439b
mswift42 <martin.haesler@gmail.com>
2017-03-21 14:33:42 +0000

    Add methods for favouritesprovider.

--- app/src/main/java/mswift42/com/github/eggcrackin/FavouritesProvider.kt
+++ app/src/main/java/mswift42/com/github/eggcrackin/FavouritesProvider.kt
@@ -1,4 +1,25 @@
 package mswift42.com.github.eggcrackin
 
 object FavouritesProvider {
+    var favourites = HashSet<Recipe>()
+
+    fun getFavourites(): List<Recipe> {
+        return favourites.toList()
+    }
+
+    fun setFavourites(favs: List<Recipe>) {
+        favourites = favs.toHashSet()
+    }
+
+    fun addFavourites(recipe: Recipe) {
+        favourites.add(recipe)
+    }
+
+    fun deleteFavourite(recipe: Recipe) {
+        favourites.remove(recipe)
+    }
+
+    fun isFavourite(recipe: Recipe) : Boolean {
+        return favourites.contains(recipe)
+    }
 }
