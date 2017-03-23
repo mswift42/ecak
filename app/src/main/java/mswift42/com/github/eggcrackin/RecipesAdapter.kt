dcaa9fcb2f90122aa5bf022c4e24d4d1c3520854
mswift42 <martin.haesler@gmail.com>
2017-03-23 12:34:38 +0000

    Add clickHandler for favourite icon.

--- app/src/main/java/mswift42/com/github/eggcrackin/RecipesAdapter.kt
+++ app/src/main/java/mswift42/com/github/eggcrackin/RecipesAdapter.kt
@@ -8,31 +8,48 @@ import kotlinx.android.synthetic.main.recipe_card.view.*
 import mswift42.com.github.eggcrackin.Recipe
 import mswift42.com.github.eggcrackin.MockRecipeData
 
-class RecipesAdapter(val Recipes : MutableList<Recipe>, val itemClick: (Recipe) -> Unit) :
+class RecipesAdapter(val Recipes : MutableList<Recipe>) :
         RecyclerView.Adapter<RecipesAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_card, parent, false)
-        return ViewHolder(view, itemClick)
+       val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_card, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder,  position: Int) {
-        holder.bindRecipe(Recipes[position])
+        val isFavourite = FavouritesProvider.isFavourite(Recipes[position])
+        holder.bindRecipe(Recipes[position], isFavourite)
 
     }
 
     override fun getItemCount() = Recipes.size
 
-    class ViewHolder(view: View, val itemClick: (Recipe) -> Unit) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View ) : RecyclerView.ViewHolder(view) {
 
-        fun bindRecipe(recipe: Recipe) {
+        fun bindRecipe(recipe: Recipe, isFavourite: Boolean) {
             with(recipe) {
                 itemView.ec_recipe_title.text = title
                 itemView.ec_recipe_image.setImageResource(image_url)
                 itemView.ec_recipe_publisher.text = publisher
-
-
+                if (isFavourite) {
+                    itemView.ec_favorite_icon_not_favourite.setImageResource(R.drawable.ic_favorite_black_24dp)
+                }
+                itemView.ec_favorite_icon_not_favourite.setOnClickListener { v ->
+                    toggleFavourite(recipe)
+                }
             }
+        }
+
+        private fun toggleFavourite(recipe: Recipe) {
+            if (FavouritesProvider.isFavourite(recipe)) {
+                itemView.ec_favorite_icon_not_favourite.setImageResource(R.drawable.ic_favorite_border_black_24dp)
+                FavouritesProvider.deleteFavourite(recipe)
+            } else {
+                itemView.ec_favorite_icon_not_favourite.setImageResource(
+                        R.drawable.ic_favorite_black_24dp)
+                FavouritesProvider.addFavourites(recipe)
+            }
+
         }
     }
 }
